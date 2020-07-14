1066d3fdd8f2baf397f61900360f4eada0d050ae
Mitsuo Yazawa <mitsuo@entermediadb.org>
2020-07-14 12:13:48 -0600

    Cleaning electron src, Adding new url, Adding a call to open workspace and adding a new windows installer

--- electron/src/index.js
+++ electron/src/index.js
@@ -8,6 +8,7 @@ const { dirname } = require("path");
 // exports to renderer
 exports.execCmd = execCmd;
 exports.startMediaBoat = startMediaBoat;
+exports.loadNewUrl = openWorkspace;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -33,9 +34,8 @@ const createWindow = () => {
     }
   });
   
-  // and load the index.html of the app.
-  // this.mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  this.mainWindow.loadURL("https://em10.entermediadb.org/assets/mediaapp/index.html");
+  // this.mainWindow.loadURL("https://em10.entermediadb.org/assets/mediaapp/index.html");
+  this.mainWindow.loadURL("https://entermediadb.org/app/workspaces/index.html");
   // this.mainWindow.loadURL("http://localhost/electron/index.html");
   
   // Open the DevTools.
@@ -83,6 +83,10 @@ const createWindow = () => {
   });
   
 };
+
+function openWorkspace(url) {
+  this.mainWindow.loadURL(url);
+}
 
 function setMainMenu(win) {
   const template = [
@@ -174,32 +178,24 @@ function createTray() {
 function checkSession(win) {
   this.session = win.webContents.session
 }
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', async () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  console.log('mediaboat.pid', this.mediaBoatClient.pid+1);
-  await process.kill(this.mediaBoatClient.pid+1);
+app.on('window-all-closed', () => {
+  process.kill(this.mediaBoatClient.pid + 1);
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+app.on('open-url', () => {
+  console.log('Open by url thingie');
+});
+
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-// background-image: url('/entermediadb/mediadb/services/module/asset/downloads/preset/2019/12/f0/94a/image200x200.png')
 
 

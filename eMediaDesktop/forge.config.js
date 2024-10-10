4f1e28f27a3232b2670f8b847ffac04cc26e847e
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2024-10-10 19:58:09 +0600

    residuals deleted

--- eMediaDesktop/forge.config.js
+++ eMediaDesktop/forge.config.js
@@ -60,8 +60,6 @@ module.exports = {
       name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
